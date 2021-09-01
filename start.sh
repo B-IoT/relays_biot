dbfef2e110cb1858d229f2239a1bbc04fe92f251
samuelchassot <samuel.chassot@epfl.ch>
2021-09-01 08:02:31 +0200

    start scripts are run by the user pi now

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 # This script is executed at the start of the relays
+# It is run with pi as user and started from rc.local
 # DO NOT MODIFY IT UNLESS YOU REALLY KNOW WHAT YOU ARE DOING!!!
 
 cd $(dirname $0)
@@ -8,5 +9,5 @@ while ! (ping -c 1 -W 1 1.2.3.4 | grep -q 'statistics'); do
     sleep 10
 done
 
-sudo -u pi git pull
+git pull
 sh start_relays.sh
